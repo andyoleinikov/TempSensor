43942c1adb0bbbf312bc536f63fd22e487c49028
Andrey Oleinikov <dyt321@ya.ru>
2017-11-04 01:54:42 +0300

    Interactive legend added to html graph

--- graph_plotter_easy.py
+++ graph_plotter_easy.py
@@ -1,4 +1,3 @@
-
 import matplotlib as mpl
 mpl.use('Agg')
 from mpld3 import plugins
@@ -7,12 +6,10 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib.ticker import FuncFormatter
 import matplotlib.font_manager as fm
-
+import datetime as dt 
+from dateutil.relativedelta import  relativedelta
 
 from tdb import get_item_range
-import datetime as dt 
-from dateutil import rrule
-from dateutil.relativedelta import  relativedelta
 from forecast import get_weather_lists
 
 def plot_forecast(ax, date):
@@ -20,6 +17,7 @@ def plot_forecast(ax, date):
     if date < dt.datetime.now()+ plot_condition_date and date > dt.datetime.now() - plot_condition_date:
         forecast_dates, forecast_temps = get_weather_lists()
         return ax.plot_date(forecast_dates, forecast_temps, '-', label='forecast')
+
 
 def plot_db_data(ax, date):
     for source in ['gismeteo', 'RPI_sensor']:
@@ -38,6 +36,7 @@ def plot_db_data(ax, date):
             date_ -= relativedelta(years=1)
             year_delta += 1
 
+
 def plot_grid(fig, ax):
     ax.xaxis.set_major_locator(mdates.DayLocator())
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
@@ -48,24 +47,29 @@ def plot_grid(fig, ax):
     fig.autofmt_xdate()
 
     ax.grid(True)
-    ax.legend()
+
 
 def get_plot(date=dt.datetime.now(), output='html'):
     
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 4))
 
     plot_db_data(ax, date)
         
     plot_forecast(ax, date)
 
     plot_grid(fig, ax)
-
     
+    handles, labels = ax.get_legend_handles_labels()
 
     if output == 'html':
+        interactive_legend = plugins.InteractiveLegendPlugin(handles, labels)
+        plugins.connect(fig, interactive_legend)
+        fig.subplots_adjust(right=0.7)
         plot_html = mpld3.fig_to_html(fig, template_type='general')
         return plot_html
+    
     else:
+        ax.legend(handles, labels)
         plt.savefig('./static/graph/graph.png')
         plt.close()
         f = open('./static/graph/graph.png', 'rb')  # some file on local disk
@@ -73,4 +77,4 @@ def get_plot(date=dt.datetime.now(), output='html'):
 
 
 if __name__ == '__main__':
-    get_plot_html(date=dt.datetime.now(), output='img')
+    get_plot(date=dt.datetime.now(), output='img')
